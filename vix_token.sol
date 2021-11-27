5984a52347b64878e25f5b71a4262fce78090856
jtengsico <83848677+jtengsico@users.noreply.github.com>
2021-11-27 05:20:12 -0800

    fixing code. adding comments about functions to finish before deployment

--- vix_token.sol
+++ vix_token.sol
@@ -4,13 +4,27 @@ pragma solidity ^0.5.0;
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20Detailed.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20Mintable.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20Capped.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/IERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20Pausable.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/ownership/Ownable.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/math/SafeMath.sol";
 
 
-//token creation 
-//edit the line below for exchange rate 
-//initial supply is 10 million tokens 
-contract VixCoin is ERC20, ERC20Detailed, ERC20Mintable {
+
+//srini please break up code into separate files
+//still need to check code for accuracy 
+//need to make token price based on performance 
+//check if users can buy and sell token
+//when users buy token it is in USD and not ether
+//need chainlink integration. node runs on computer locally. not sure if contract owner needs libraries installed on their own machine to deploy chainlink portion of contract 
+
+//token_price_today = token price yesterday*(1+vixm_return_yesterday*signal_hat_yesterday) - in USD 
+
+
+//only deployer can mint tokens
+//set cap of tokens at 10000000
+contract VixCoin is ERC20, ERC20Detailed, ERC20Mintable, ERC20Capable {
     constructor(
         string memory name,
         string memory symbol,
@@ -19,8 +33,25 @@ contract VixCoin is ERC20, ERC20Detailed, ERC20Mintable {
         ERC20Detailed(VixCoin, VIX, 18)
         public
     {
+        //only deployer can mint tokens
         mint(msg.sender, initial_supply);
     }
+
+//only owner can mint new tokens and transfer ownership
+//contact owner is the one who deploys
+//owner can transfer contract to another
+contract VixCoin is Ownable {
+    constructor(
+
+    )
+
+
+
+
+}
+
+
+// set token price 
 
 contract VixCoin {
     using SafeMath for uint;
@@ -28,11 +59,11 @@ contract VixCoin {
     address payable owner = msg.sender;
     string public symbol = "VIX";
 
-    //exchange rate should not be public and should update daily
-    //exchange rate is cost of token
-    //only owner can update?
+    //token price should not be public and should update daily
+    //token price is cost of token
+    //only owner can update 
 
-    uint public exchange_rate = ;
+    uint token_price = ;
     mapping(address => uint) balances;
 
     function balance() public view returns(uint) {
@@ -51,7 +82,7 @@ contract VixCoin {
         owner.transfer(msg.value);
     }
 
-    //log of exchange rates
+    //user log of buy and sell?
     function log()
 
     function mint(address recipient, uint value) public {
@@ -59,5 +90,7 @@ contract VixCoin {
         balances[recipient] = balances[recipient].add(value);
     }
 }
+
+
 
 
